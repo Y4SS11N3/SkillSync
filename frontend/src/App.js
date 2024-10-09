021367abd805d68f3f0a9fdd2631875b9433ebaf
YASSINE <contact@yassine.fun>
2024-10-09 17:03:30 +0100

    Add ConditionalFooter to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import ConditionalFooter from './components/Footer/ConditionalFooter';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+      <div className="App flex flex-col min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 transition-colors duration-200">
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+        <ConditionalFooter />
       </div>
     </Router>
   );
