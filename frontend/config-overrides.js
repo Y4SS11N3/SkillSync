8f677ed23ede3b2d0bde8986d426ba27dbc81773
YASSINE <contact@yassine.fun>
2024-10-09 16:57:00 +0100

    Fix compilation errors and start development server

---
+++ frontend/config-overrides.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  // Add polyfills
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "crypto": require.resolve("crypto-browserify"),
+    "stream": require.resolve("stream-browserify"),
+    "buffer": require.resolve("buffer/"),
+    "process": require.resolve("process/browser"),
+  };
+
+  // Add plugins
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ];
+
+  // Handle fully specified module resolution
+  config.module.rules.push({
+    test: /\.m?js/,
+    resolve: {
+      fullySpecified: false
+    }
+  });
+
+  return config;
+}
