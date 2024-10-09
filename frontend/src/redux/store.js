2d840dcd6c384886f9aac7a83ec8c168f1122eeb
YASSINE <contact@yassine.fun>
2024-10-09 18:36:11 +0100

    Add skillReducer to redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './reducers/authReducer';
 import userReducer from './reducers/userReducer';
+import skillReducer from './reducers/skillReducer';
 
 export default configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    skills: skillReducer,
   },
 });
