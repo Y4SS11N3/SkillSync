e77fbb59829b4db11309f9630fa65c79caa50f10
YASSINE <contact@yassine.fun>
2024-10-10 20:29:36 +0100

    Implement Chat page for user messaging

---
+++ frontend/src/pages/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatContent from '../components/Chat/ChatContent';
+
+const Chat = () => {
+  return (
+      <div className="flex h-screen bg-gray-100">
+        <ChatContent />
+      </div>
+  );
+};
+
+export default Chat;
